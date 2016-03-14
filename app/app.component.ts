a8f87cebe7cdb20703d700d4a31325897f4c9a40
Bo-Duke <maxime.bouveron@gmail.com>
2016-03-14 15:26:00 +0100

    Just the view

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,12 @@ import {Component} from 'angular2/core';
 })
 export class AppComponent {
     calcul: string = "";
-    printNumber(numero: number){
-        this.calcul += String(numero);
+    result: string = "";
+    buttons: string[] = ['1','2','3','4','5','6','7','8','9','0','+'];
+    printNumber(numero: string){
+        this.calcul += numero;
+    }
+    printResult(){
+        this.result = this.calcul;
     }
 }
